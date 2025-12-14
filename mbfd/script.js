44975ac63663065b4e867cad05100f49108a9d23
Acturaleta <acturaleta@gmail.com>
2025-12-14 10:34:09 +0100

    Add files via upload

---
+++ mbfd/script.js
@@ -0,0 +1,2 @@
+document.title = "Metro de Barcelona, flota detallada";
+document.body.hidden = false;
---
+++ mmfd/script.js
@@ -0,0 +1,2 @@
+document.title = "Metro de Madrid, flota detallada";
+document.body.hidden = false;
